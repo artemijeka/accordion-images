80e1f670b3c9121b43df47df556ed0b7eddc00c9
Artem Kunecov <artem.kuznecov.samara@yandex.ru>
2022-08-27 22:22:09 +0400

    upd

--- script.js
+++ script.js
@@ -2,25 +2,28 @@
   document.addEventListener('DOMContentLoaded', function () {
 
 
+    function accordionImages(indexActiveSlide = 0) {
+      const slides = document.querySelectorAll('.slide')
 
-    const slides = document.querySelectorAll('.slide')
-    console.log(slides)
+      slides[indexActiveSlide].classList.add('--active')
 
-    slides.forEach(function (slide) {
-      slide.addEventListener('click', () => {
-
-        clearActiveClasses()
-
-        slide.classList.add('--active')
-      })
-    })
-
-    function clearActiveClasses() {
       slides.forEach(function (slide) {
-        slide.classList.remove('--active')
-      });
+        slide.addEventListener('click', () => {
+
+          clearActiveClasses()
+
+          slide.classList.add('--active')
+        })
+      })
+
+      function clearActiveClasses() {
+        slides.forEach(function (slide) {
+          slide.classList.remove('--active')
+        });
+      }
     }
 
+    accordionImages(0)
 
 
   })
